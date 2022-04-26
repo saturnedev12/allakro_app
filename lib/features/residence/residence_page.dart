43c8d3a14c24c1db058c293b8db0fa4a2cb35550
Wognin Ayoua <60236973+saturnedev12@users.noreply.github.com>
2022-04-26 17:44:18 +0000

    domicile page

--- lib/features/residence/residence_page.dart
+++ lib/features/residence/residence_page.dart
@@ -18,25 +18,116 @@ class _ResidencePageState extends State<ResidencePage> {
           padding: const EdgeInsets.only(left: 8, right: 8, top: 5),
           child: Column(
             children: [
-              Card(
-                child: Container(
-                  width: double.infinity,
-                  height: 300,
-                  child: Column(
-                    children: [
-                      Row(
-                        children: [
-                          SizedBox(
-                            child: Icon(
+              for (var i = 0; i < 7; i++)
+                Card(
+                  margin: EdgeInsets.only(top: 20),
+                  child: Container(
+                    padding: EdgeInsets.all(10),
+                    width: double.infinity,
+                    height: 300,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: const [
+                            Chip(
+                              backgroundColor: Colors.redAccent,
+                              label: Text(
+                                'Occupé',
+                                style: TextStyle(color: Colors.white),
+                              ),
+                            ),
+                            Icon(
                               CupertinoIcons.house_fill,
                             ),
+                          ],
+                        ),
+                        Text(
+                          'Mainson 3 piece',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 22,
                           ),
-                        ],
-                      ),
-                    ],
+                        ),
+                        Row(
+                          children: [
+                            Icon(
+                              CupertinoIcons.location,
+                              color: Colors.grey,
+                            ),
+                            Text(
+                              'Allakro/ zone du nord prêt de la cheferie',
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                color: Colors.grey,
+                                fontSize: 12,
+                              ),
+                            ),
+                          ],
+                        ),
+                        Row(
+                          children: [
+                            Icon(CupertinoIcons.person_crop_circle),
+                            RichText(
+                                text: TextSpan(
+                              style: TextStyle(color: Colors.black),
+                              children: <TextSpan>[
+                                TextSpan(
+                                  text: 'Propriétaire: ',
+                                ),
+                                TextSpan(
+                                  text: 'Koffi Jean Setone',
+                                  style: TextStyle(fontWeight: FontWeight.bold),
+                                ),
+                              ],
+                            ))
+                          ],
+                        ),
+                        Row(
+                          children: [
+                            Icon(CupertinoIcons.calendar),
+                            RichText(
+                                text: TextSpan(
+                              style: TextStyle(color: Colors.black),
+                              children: <TextSpan>[
+                                TextSpan(
+                                  text: 'Date aménagement: ',
+                                ),
+                                TextSpan(
+                                  text: '10/04/2009',
+                                  style: TextStyle(fontWeight: FontWeight.bold),
+                                ),
+                              ],
+                            ))
+                          ],
+                        ),
+                        Row(
+                          children: [
+                            Icon(CupertinoIcons.calendar),
+                            RichText(
+                                text: TextSpan(
+                              style: TextStyle(color: Colors.black),
+                              children: <TextSpan>[
+                                TextSpan(
+                                  text: 'Date précédent déménagement: ',
+                                ),
+                                TextSpan(
+                                  text: '10/04/2009',
+                                  style: TextStyle(fontWeight: FontWeight.bold),
+                                ),
+                              ],
+                            ))
+                          ],
+                        ),
+                        Text(
+                          '''Une maison est, en droit civil français, un immeuble, mot qui désigne aussi couramment un édifice de plusieurs étages divisé en plusieurs appartements occupés ...''',
+                        )
+                      ],
+                    ),
                   ),
                 ),
-              ),
             ],
           )),
     );
